0a822f24914b28ec43d5b0ef273f4cd1a320905d
vaibhavagrawal0289 <vaibhav.agrawal0289@gmail.com>
2017-09-12 21:03:13 -0700

    Integrated UI and latest changes for neutron logs

--- routes/neutron.js
+++ routes/neutron.js
@@ -22,7 +22,7 @@ client.ping({
 
 /* GET home page. */
 
-function fetchNeutronLogs(req, res){
+function fetchNeutronLogs(req,res){
     var MyDate = new Date();
     var isoDate = new Date(MyDate).toISOString();
     var MyDateString;
@@ -31,7 +31,6 @@ function fetchNeutronLogs(req, res){
     var month = dateObj.getUTCMonth() + 1; //months from 1-12
     var day = dateObj.getUTCDate();
     var year = dateObj.getUTCFullYear();
-
 
     MyDateString =year+'.'+('0' + month).slice(-2)+'.'+('0' + day).slice(-2);
     console.log(MyDateString);
@@ -47,17 +46,28 @@ function fetchNeutronLogs(req, res){
     }).then(function (body) {
         var hits=body.hits.hits;
         var result=[];
+
+        //Creating a json object with timestamp, loglevel and message
+        var jsonObject={};
         for (var i=0; i<hits.length;i++){
-            result.push(stripAnsi(hits[i]._source.message));
+            var strippedMessage= stripAnsi(hits[i]._source.message[1]);
+
+            var loglevel=strippedMessage.substr(0,strippedMessage.indexOf(' '));
+            var message=strippedMessage.substr(strippedMessage.indexOf(' ')+1);
+
+            jsonObject={"timestamp":hits[i]._source.timestamp, "loglevel":loglevel, "message":message};
+            result.push(jsonObject);
         }
+
         //Since i was getting unicode and ansi code characters with the message i am striping those
         //so we can show only the ascii characters on the UI
-        console.log(stripAnsi(hits));
-        console.log("Successfull");
+        //console.log(stripAnsi(hits));
+        console.log("Successful");
         res.send(JSON.stringify(result));
     }, function (error) {
         console.trace(error.message);
     });
+
 
 }
 
@@ -65,8 +75,7 @@ exports.fetchNeutronLogs=fetchNeutronLogs;
 
 
 function neutron(req,res){
-    //Set these headers to notify the browser not to maintain any cache for the page being loaded
-    res.render("neutron-dhcp",{username:"abc"});
+    res.render("Neutron.ejs",{username:"abc"});
 }
 
 
